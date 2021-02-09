441c4219c24d9d1ee37594e1890460807c3de50b
alexhstrickland <alexhstrickland@gmail.com>
2021-02-09 11:09:00 -0500

    Added changes to generate Markdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,7 +13,10 @@ function renderLicenseBadge(license) {
       response = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
     case 'Apache 2.0':
       response = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
+    case 'None':
+      response = "";
   }
+  return response;
 }
 
 // TODO: Create a function that returns the license link
@@ -31,12 +34,23 @@ function renderLicenseLink(license) {
       response = "(https://www.gnu.org/licenses/gpl-3.0)";
     case 'Apache 2.0':
       response = "(https://opensource.org/licenses/Apache-2.0)";
+    case 'None':
+      response = "";
   }
+  return response;
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
+
+  if (!license == "") {
+    return `## License
+  This project is covered under the ${data.license} license. Visit the following link for more information on this license: [${data.license}]${renderLicenseLink(data.license)}`;
+  } else {
+    return "";
+  }
+
 }
 
 // TODO: Create a function to generate markdown for README
@@ -89,3 +103,4 @@ function generateMarkdown(data) {
 }
 
 module.exports = generateMarkdown;
+s
