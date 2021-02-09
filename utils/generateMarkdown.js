6514530589a681013cbade53bc9f33689ba11d07
alexhstrickland <alexhstrickland@gmail.com>
2021-02-08 22:56:02 -0500

    added links to render license link section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -22,15 +22,15 @@ function renderLicenseLink(license) {
   const input = license;
   switch(input) {
     case 'ISC':
-      response = [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+      response = "(https://opensource.org/licenses/ISC)";
     case 'MIT':
-      response = [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+      response = "(https://opensource.org/licenses/MIT)";
     case 'BSD 3':
-      response = [![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+      response = "(https://opensource.org/licenses/BSD-3-Clause)";
     case 'GPL 3.0':
-      response = [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
-    case 'Apache 2.0'
-      response = [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+      response = "(https://www.gnu.org/licenses/gpl-3.0)";
+    case 'Apache 2.0':
+      response = "(https://opensource.org/licenses/Apache-2.0)";
   }
 }
 
