9643e19c812989334bec06018e02014e29158eb4
alexhstrickland <alexhstrickland@gmail.com>
2021-02-08 22:59:35 -0500

    added links to badges in render license badge section

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,15 +4,15 @@ function renderLicenseBadge(license) {
   const input = license;
   switch(input) {
     case 'ISC':
-      response = [![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+      response = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]";
     case 'MIT':
-      response = [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+      response = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]";
     case 'BSD 3':
-      response = [![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+      response = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]";
     case 'GPL 3.0':
-      response = [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
-    case 'Apache 2.0'
-      response = [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+      response = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]";
+    case 'Apache 2.0':
+      response = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]";
   }
 }
 
